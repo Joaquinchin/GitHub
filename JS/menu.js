e8562864108c312190638733624e9738e1379273
Joaquin Ordonez <joacoignacioordonez@gmail.com>
2023-11-01 20:48:45 -0300

    Agregamos el menu de navagacion toton gay

--- JS/menu.js
+++ JS/menu.js
@@ -4,4 +4,8 @@ const MenuNav = document.querySelector('.menu-navegacion')
 console.log(Menu)
 console.log(MenuNav)
 
-Menu.addEventListener('click')
+Menu.addEventListener('click', ()=>{/*Funcio de flecha*/
+    MenuNav.classList.toggle("spreed")/*Lo que hace esta funcion es que le agrega es clase a la otra clase, en este caso a la 
+    clase Menu navegacion, le agregagomos el spreed para que cuando hagamos click se muestre el menu nav, que lo teniamos oculto*/
+    /*toggle, le agrega o le saca una clase a otra dependiendo si la tiene o no nashei*/
+})
